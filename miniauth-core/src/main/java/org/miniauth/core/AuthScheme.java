c33c94e0eafa9a7294f951916a9c114fe93d5557
Harry Y <harrywye@gmail.com>
2013-10-14 20:24:07 -0700

    Added a helper method for AuthScheme.

--- miniauth-core/src/main/java/org/miniauth/core/AuthScheme.java
+++ miniauth-core/src/main/java/org/miniauth/core/AuthScheme.java
@@ -6,9 +6,30 @@ public final class AuthScheme
 {
     public static final String BASIC = "Basic";
     public static final String DIGEST = "Digest";
-    public static final String OAuth = "OAuth";
+    public static final String OAUTH = "OAuth";
+    public static final String OAUTH2 = "OAuth2";
+    // etc. ...
+    
+    public static final String TOKEN_TYPE_BEARER = "Beaer";
     // etc. ...
    
     private AuthScheme() {}
+    
+    
+    // temporary
+    // Returns the auth name that is used right after "Authorization: ".
+    // The reason for this function is that OAuth2 uses "Bearer" scheme (currently),
+    //      which is different from the auth scheme name.
+    // TBD: Is there a better name for this function?
+    public static String getAuthorizationHeaderAuthScheme(String authScheme)
+    {
+        switch(authScheme) {
+        case OAUTH2:
+            return TOKEN_TYPE_BEARER;
+        default:
+            return authScheme;
+        }
+    }
+
 
 }
