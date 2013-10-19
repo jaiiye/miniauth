de9f3d14486d99d3f380a511079e7ca5436650cd
Harry Y <harrywye@gmail.com>
2013-10-19 16:36:38 -0700

    Implemented high-level Endorser/Verifier services.

--- miniauth-oauth/src/main/java/org/miniauth/oauth/service/OAuthEndorserService.java
+++ miniauth-oauth/src/main/java/org/miniauth/oauth/service/OAuthEndorserService.java
@@ -4,7 +4,10 @@ import java.util.logging.Logger;
 
 import org.miniauth.MiniAuthException;
 import org.miniauth.common.OutgoingRequest;
+import org.miniauth.credential.AccessCredential;
+import org.miniauth.credential.AccessIdentity;
 import org.miniauth.oauth.callback.OAuthCredentialMapper;
+import org.miniauth.oauth.common.OAuthOutgoingRequest;
 import org.miniauth.service.EndorserService;
 
 
@@ -12,18 +15,26 @@ public class OAuthEndorserService extends OAuthCredentialService implements Endo
 {
     private static final Logger log = Logger.getLogger(OAuthEndorserService.class.getName());
 
+    // private OAuthRequestEndorser requestEndorser = null;
+    
     public OAuthEndorserService(OAuthCredentialMapper credentialMapper)
     {
         super(credentialMapper);
+        // requestEndorser = OAuthRequestEndorser.getInstance();
+    }
+
+    public OAuthCredentialMapper getOAuthCredentialMapper()
+    {
+        return (OAuthCredentialMapper) getCredentialMapper();
     }
 
     @Override
     public boolean endorse(OutgoingRequest request) throws MiniAuthException
     {
-        // TBD
-        return false;
+        AccessIdentity accessIdentity = ((OAuthOutgoingRequest) request).getAccessIdentity();
+        AccessCredential credential = getOAuthCredentialMapper().getAccesssCredential(accessIdentity);
+        return OAuthRequestEndorser.getInstance().endorse(credential, request);
     }
-
     
     
 }
--- miniauth-oauth/src/main/java/org/miniauth/oauth/service/OAuthVerifierService.java
+++ miniauth-oauth/src/main/java/org/miniauth/oauth/service/OAuthVerifierService.java
@@ -4,7 +4,10 @@ import java.util.logging.Logger;
 
 import org.miniauth.MiniAuthException;
 import org.miniauth.common.IncomingRequest;
+import org.miniauth.credential.AccessCredential;
+import org.miniauth.credential.AccessIdentity;
 import org.miniauth.oauth.callback.OAuthCredentialMapper;
+import org.miniauth.oauth.common.OAuthIncomingRequest;
 import org.miniauth.service.VerifierService;
 
 
@@ -12,16 +15,25 @@ public class OAuthVerifierService extends OAuthCredentialService implements Veri
 {
     private static final Logger log = Logger.getLogger(OAuthVerifierService.class.getName());
 
+    // private OAuthRequestVerifier requestVerifier = null;
+
     public OAuthVerifierService(OAuthCredentialMapper credentialMapper)
     {
         super(credentialMapper);
+        // requestVerifier = OAuthRequestVerifier.getInstance();
+    }
+
+    public OAuthCredentialMapper getOAuthCredentialMapper()
+    {
+        return (OAuthCredentialMapper) getCredentialMapper();
     }
 
     @Override
     public boolean verify(IncomingRequest request) throws MiniAuthException
     {
-        // TBD
-        return false;
+        AccessIdentity accessIdentity = ((OAuthIncomingRequest) request).getAccessIdentity();
+        AccessCredential credential = getOAuthCredentialMapper().getAccesssCredential(accessIdentity);
+        return OAuthRequestVerifier.getInstance().verify(credential, request);
     }
 
     
